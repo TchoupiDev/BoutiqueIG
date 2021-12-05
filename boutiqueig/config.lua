c0d13f4878aed92feb28136bd3203bbbecf93167
Tchoupi <86742056+TchoupiDev@users.noreply.github.com>
2021-12-05 16:00:27 +0100

    Add files via upload

---
+++ boutiqueig/config.lua
@@ -0,0 +1,17 @@
+----------------------- license ou steam
+
+id_system_l_o_s = "steam"
+
+----------------- nom de la money virtuelle
+
+moneypoints = "~g~Coin"
+
+-------------------------- nom de votre resources concesionaire pour l'export Generateplate
+
+exportvecleshop = "esx_vehicleshop"
+
+------------ défault touche F1 = 288
+
+touche_open_menu = 288
+
+--besoins d'aide: https://discord.gg/fExvvxArG3
