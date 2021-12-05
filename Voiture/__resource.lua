c1d7084d4bfa6dfa89c8b5390d3eca6fa48aa4ec
Tchoupi <86742056+TchoupiDev@users.noreply.github.com>
2021-12-05 20:53:15 +0100

    Add files via upload

---
+++ Voiture/__resource.lua
@@ -0,0 +1,122 @@
+description 'Fordsamu Script'
+
+client_script  {
+}
+-- Tmax
+files {
+  'tmax/handling.meta',
+  'tmax/vehicles.meta',
+  'tmax/carcols.meta',
+  'tmax/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'tmax/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'tmax/vehicles.meta'
+data_file 'CARCOLS_FILE' 'tmax/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'tmax/carvariations.meta'
+
+
+-- RS3
+files {
+  'rs318/handling.meta',
+  'rs318/vehicles.meta',
+  'rs318/carcols.meta',
+  'rs318/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'rs318/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'rs318/vehicles.meta'
+data_file 'CARCOLS_FILE' 'rs318/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'rs318/carvariations.meta'
+
+-- RS6 
+files {
+  'rs6avant20/handling.meta',
+  'rs6avant20/vehicles.meta',
+  'rs6avant20/carcols.meta',
+  'rs6avant20/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'rs6avant20/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'rs6avant20/vehicles.meta'
+data_file 'CARCOLS_FILE' 'rs6avant20/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'rs6avant20/carvariations.meta'
+
+-- RS5
+files {
+  'rs5r/handling.meta',
+  'rs5r/vehicles.meta',
+  'rs5r/carcols.meta',
+  'rs5r/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'rs5r/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'rs5r/vehicles.meta'
+data_file 'CARCOLS_FILE' 'rs5r/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'rs5r/carvariations.meta'
+
+-- Q7
+files {
+  'Q7/handling.meta',
+  'Q7/vehicles.meta',
+  'Q7/carcols.meta',
+  'Q7/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'Q7/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'Q7/vehicles.meta'
+data_file 'CARCOLS_FILE' 'Q7/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'Q7/carvariations.meta'
+
+
+-- Golf 7r
+files {
+  'golf75r/handling.meta',
+  'golf75r/vehicles.meta',
+  'golf75r/carcols.meta',
+  'golf75r/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'golf75r/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'golf75r/vehicles.meta'
+data_file 'CARCOLS_FILE' 'golf75r/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'golf75r/carvariations.meta'
+
+
+-- Mercedes CLS 53
+files {
+  'cls53/handling.meta',
+  'cls53/vehicles.meta',
+  'cls53/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'cls53/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'cls53/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'cls53/carvariations.meta'
+
+
+-- Camaro
+files {
+  '21camaro/handling.meta',
+  '21camaro/vehicles.meta',
+  '21camaro/carcols.meta',
+  '21camaro/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' '21camaro/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '21camaro/vehicles.meta'
+data_file 'CARCOLS_FILE' '21camaro/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '21camaro/carvariations.meta'
+
+-- SRT8
+files {
+  'srt8/handling.meta',
+  'srt8/vehicles.meta',
+  'srt8/carcols.meta',
+  'srt8/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'srt8/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'srt8/vehicles.meta'
+data_file 'CARCOLS_FILE' 'srt8/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'srt8/carvariations.meta'
